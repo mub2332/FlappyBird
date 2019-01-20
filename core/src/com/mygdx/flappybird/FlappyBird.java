436cc78741b5ced7ade127191119facb0fc7719f
mub2332 <43363470+mub2332@users.noreply.github.com>
2019-01-20 11:50:03 +1100

    rendered pipes

--- core/src/com/mygdx/flappybird/FlappyBird.java
+++ core/src/com/mygdx/flappybird/FlappyBird.java
@@ -2,19 +2,21 @@ package com.mygdx.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-
+	Texture topTube;
+	Texture bottomTube;
 	Texture[] birds;
+
 	int flapState = 0;
 	float birdY = 0;
 	float velocity = 0;
 	float gravity = 2;
+	float tubeGap = 400;
 
 	int gameState = 0;
 	
@@ -22,16 +24,30 @@ public class FlappyBird extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+
 		birds = new Texture[2];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 		birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
+
+		topTube = new Texture("toptube.png");
+		bottomTube = new Texture("bottomtube.png");
 	}
 
 	@Override
 	public void render () {
 
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         if (gameState != 0) {
+
+            batch.draw(topTube,
+                Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 + tubeGap / 2);
+            batch.draw(bottomTube,
+                Gdx.graphics.getWidth() / 2 - bottomTube.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - tubeGap / 2 - bottomTube.getHeight());
 
             if (Gdx.input.justTouched()) {
                 velocity = -30;
@@ -55,8 +71,6 @@ public class FlappyBird extends ApplicationAdapter {
             flapState = 0;
         }
 
-        batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(birds[flapState],
             Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2,
             birdY);
